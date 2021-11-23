eed7066d017de3c444ea726225dc6f02f37971f1
cwdaggs <cwdaggs@ucdavis.edu>
2021-11-23 13:31:03 -0800

    First two done

--- part1.pl
+++ part1.pl
@@ -5,8 +5,10 @@ Examples:
 • concat([1, [2, 3]], [4], [1, 2, 3, 4]) is false.
 • concat([1, [2, 3]], [4], [1, [2, 3], 4]) is true.
 You may assume the given arguments are always lists.
+https://www.cis.upenn.edu/~matuszek/Concise%20Guides/Concise%20Prolog.html
 */
-concat(L1, L2, L) :- append(L1, L2, L).
+concat([], L, L).
+concat([A | L1], L2, [A | L]) :- concat(L1, L2, L).
 
 /*
 element_at(X, N, L): meaning the element X is the Nth element in list L.
@@ -15,11 +17,17 @@ Examples:
 • element_at(2, 1, [3, 2, 4]) is false.
 • element_at(2, 1, [[2], 3, 4]) is false.
 • element_at(5, 2, [3, 2, 4]) is false.
+https://stackoverflow.com/questions/12939425/prolog-access-specific-member-of-list
 */
-element_at(X, N, L) :- nth1(N, L, X).
-
+element_at(X, N, L) :- element_at(X, 1, N, L).
+element_at(X, N, N, [X|_]) :- !.
+element_at(X, C, N, [_|L]) :- 
+    C < N,
+    C1 is C + 1,
+    element_at(X, N, C1, L).
+    
 /*
-reverse(L1, L2): meaning the reverse of list L1 is L2.
+my_reverse(L1, L2): meaning the reverse of list L1 is L2.
 Examples:
 • reverse([1,2,3], [3,2,1]) is true.
 • reverse([1,2,3], [2,1,3]) is false.
@@ -30,12 +38,13 @@ Examples:
 % reverse(L1, L2) :- 
 
 /*
-flatten(L1, L2): Given a list L1, its flattened version is L2.
+my_flatten(L1, L2): Given a list L1, its flattened version is L2.
 Examples:
 • flatten([1, 2, 3], [1, 2, 3]) is true.
 • flatten([1, [2, 3]], [1, 2, 3]) is true.
 • flatten([1, [2], [3, 4]], [1, 2, 3, 4]) is true.
 • flatten([1, [2, 3, [4, 5], 6]], [1, 2, 3, 4, 5, 6]) is true.
+ Can use append here
 */
 
 
