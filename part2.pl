18dcc071a024b46451908bc9e066b7eda8f77c97
cwdaggs <cwdaggs@ucdavis.edu>
2021-11-24 11:36:36 -0800

    Worked on some part2

--- part2.pl
+++ part2.pl
@@ -12,3 +12,36 @@ Examples:
 • Given a 4 ×4 board, there are two ways to place the 4 queens: [2, 4, 1, 3] and [3, 1, 4, 2].
 */
 
+/*
+Have to maintain the columns placed and positive diagonals and negative diagonals
+Negative diagonal = left to right, top to bottom; row - column remains constant, bounds [-(n-1), n-1]
+Positive diagonal = left to right, top to bottom; r+c remains constant, bounds [0, N+2]
+*/
+
+
+% check pos_diag()
+% check_column(N, Q, R, C, X, Y, Z) :- 
+nonmember(Arg,[Arg|_]) :-
+        !,
+        fail.
+nonmember(Arg,[_|Tail]) :-
+        !,
+        nonmember(Arg,Tail).
+nonmember(_,[]).
+
+
+% 1 and 1 are row and column counters, first list is for col, 2nd is for pos diag, 3rd is for neg diag
+queens(N, Q) :- queens(N, Q, 1, 1, [], [], []).
+% Row out of bounds, succesful list
+queens(N, Q, R, C, X, Y, Z) :- 
+    N1 is N + 1, 
+    N1 = R.
+% Column out of bounds, move to next row
+queens(N, Q, R, C, X, Y, Z) :- 
+    C1 is N + 1,
+    C1 = C,
+    R1 is R + 1,
+    queens(N, Q, R1, 1, X, Y, Z).
+queens(N, Q, R, C, X, Y, Z) :- 
+    Pos is R + C,
+    Neg is R - C,
