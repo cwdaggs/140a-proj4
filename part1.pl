b1c19d08ff6bd70f14ae1e015ba5f540c26a1c0e
cdaggs <wmanu2013@gmail.com>
2021-11-23 16:51:44 -0800

    Reverse done

--- part1.pl
+++ part1.pl
@@ -22,28 +22,32 @@ https://stackoverflow.com/questions/12939425/prolog-access-specific-member-of-li
 element_at(X, N, L) :- element_at(X, 1, N, L).
 element_at(X, N, N, [X|_]) :- !.
 element_at(X, C, N, [_|L]) :- 
-    C < N,
+    N > C,
     C1 is C + 1,
     element_at(X, N, C1, L).
     
 /*
 my_reverse(L1, L2): meaning the reverse of list L1 is L2.
 Examples:
-• reverse([1,2,3], [3,2,1]) is true.
-• reverse([1,2,3], [2,1,3]) is false.
-• reverse([1,[2,3],4,[5]], [[5],4,[2,3],1]) is true.
-• reverse([1,[2,3],4,[5]], [[5],4,[3,2],1]) is false.
-• reverse([], []) is true.
+• my_reverse([1,2,3], [3,2,1]) is true.
+• my_reverse([1,2,3], [2,1,3]) is false.
+• my_reverse([1,[2,3],4,[5]], [[5],4,[2,3],1]) is true.
+• my_reverse([1,[2,3],4,[5]], [[5],4,[3,2],1]) is false.
+• my_reverse([], []) is true.
 */
-% reverse(L1, L2) :- 
+my_reverse([], []). 
+my_reverse(L1, L2) :- reverse_acc(L1, L2, []).
+reverse_acc(L, L, []).
+reverse_acc([], L, L).
+reverse_acc([H|T], L2, L):-  reverse_acc(T, L2, [H|L]).
 
 /*
 my_flatten(L1, L2): Given a list L1, its flattened version is L2.
 Examples:
-• flatten([1, 2, 3], [1, 2, 3]) is true.
-• flatten([1, [2, 3]], [1, 2, 3]) is true.
-• flatten([1, [2], [3, 4]], [1, 2, 3, 4]) is true.
-• flatten([1, [2, 3, [4, 5], 6]], [1, 2, 3, 4, 5, 6]) is true.
+• my_flatten([1, 2, 3], [1, 2, 3]) is true.
+• my_flatten([1, [2, 3]], [1, 2, 3]) is true.
+• my_flatten([1, [2], [3, 4]], [1, 2, 3, 4]) is true.
+• my_flatten([1, [2, 3, [4, 5], 6]], [1, 2, 3, 4, 5, 6]) is true.
  Can use append here
 */
 
